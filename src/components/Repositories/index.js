7b30f4e5aee78d831bfd51253ebc6f7132921c5a
GustBitencourt <gubiten@gmail.com>
2022-01-13 14:14:28 -0300

    repositories logica map e context

--- src/components/Repositories/index.js
+++ src/components/Repositories/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import * as S from './styled';
+import React, { useEffect, useState } from 'react';
+import useGithub from '../../hooks/github-hooks';
 import ItemRepository from '../ItemRepository';
+import * as S from './styled';
 
 const Repositories = () => {
-    
+    const { githubState, getUserRepos } = useGithub();
+    const [hasUserForSearchRepos, setHasUserForSearchRepos] = useState(false);
+
+    useEffect(() => {
+        /* !! transforma login em booleano / se existir as tabs serão carregadas */
+        if(!!githubState.user.login) {
+            getUserRepos();
+        }
+        setHasUserForSearchRepos(!!githubState.repositories);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [githubState.user])
+
 
     return (
+        <>
+        {hasUserForSearchRepos ? (
         <S.WrapperTabs
             selectedTabClassName="is-selected"
             selectedTabPanelClassName="is-selected"
@@ -16,22 +31,35 @@ const Repositories = () => {
                 <S.WrapperTab>Starred</S.WrapperTab>
 
             </S.WrapperTabList>
+
             <S.WrapperTabPanel>
-                <ItemRepository
-                    name="repo1"
-                    linkToRepo="https://github.com/GustBitencourt"
-                    fullName="GustBitencourt/Blabla"
-                />
+                {githubState.repositories.map((item) => (
+                    <ItemRepository
+                        key={item.id}
+                        name={item.name}
+                        linkToRepo={item.full_name}
+                        fullName={item.full_name}
+                    />
+                ))}                
             </S.WrapperTabPanel>
 
             <S.WrapperTabPanel>
-                <ItemRepository
-                    name="repo2"
-                    linkToRepo="https://github.com/GustBitencourt"
-                    fullName="GustBitencourt/Blabla"
-                />
+                {githubState.starred.map((item) => (
+                    <ItemRepository
+                        key={item.id}
+                        name={item.name}
+                        linkToRepo={item.full_name}
+                        fullName={item.full_name}
+                    />
+                ))}                
             </S.WrapperTabPanel>
-        </S.WrapperTabs>
+        </S.WrapperTabs>) : 
+
+        /* Se hasUserForSearchRepos for falso mostra fragmento vazio */
+        ( <></>)
+        }
+        </>
+
 
     );
 }
